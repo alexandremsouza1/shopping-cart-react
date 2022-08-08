32c0fd6a01d0a5c6a0f50f8fe42305eb1dd355fa
Alexandre Magno <pedestaljunior@gmail.com>
2022-08-08 16:18:14 -0300

    menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,13 @@
-import { MenuIcon, SearchIcon, ShoppingBagIcon } from '@heroicons/react/outline';
+import { ChevronDownIcon, UserAddIcon, UserIcon, MenuIcon, SearchIcon, ShoppingBagIcon, TranslateIcon } from '@heroicons/react/outline';
+import i18n from 'i18next';
+import { useState } from 'react';
 
 export const Header = () => {
+  const changeLanguage = (lng : string) => {
+    i18n.changeLanguage(lng);
+  }
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="relative bg-white">
       <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
@@ -29,25 +36,43 @@ export const Header = () => {
 
             <div className="ml-auto flex items-center">
               <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
-                  Sign in
-                </a>
+                  <div className="flex">
+                    <UserIcon className="h-6 text-gray-700" aria-hidden="true" />
+                    <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                      Sign in
+                    </a>
+                  </div>
                 <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
-                <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
-                  Create account
-                </a>
-              </div>
-
-              <div className="hidden lg:ml-8 lg:flex">
-                <a href="#" className="flex items-center text-gray-700 hover:text-gray-800">
-                  <img
-                    src="https://tailwindui.com/img/flags/flag-canada.svg"
-                    alt=""
-                    className="block h-auto w-5 flex-shrink-0"
-                  />
-                  <span className="ml-3 block text-sm font-medium">CAD</span>
-                  <span className="sr-only">, change currency</span>
-                </a>
+                <div className="flex">
+                    <UserIcon className="h-6 text-gray-700" aria-hidden="true" />
+                    <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                      UserAddIcon
+                    </a>
+                  </div>
+                <div className="flex relative">
+                  <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  <TranslateIcon className="h-6 text-gray-700" aria-hidden="true" />
+                  <a id="dropdownDefault" data-dropdown-toggle="dropdown" href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                    Translate
+                  </a>
+                  <ChevronDownIcon className="h-6 text-gray-700" aria-hidden="true"  onClick={() => setIsOpen(!isOpen)}/>
+                  {isOpen && (
+                     //submenu with tailwind
+                    <div className="absolute mt-5 w-48 rounded-md shadow-lg">
+                      <div className=" bg-white rounded-md shadow-xs">
+                        <div className="py-1">
+                          <a href="#" className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" onClick={() => changeLanguage('en')}>
+                            English
+                          </a>
+                          <a href="#" className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" onClick={() => changeLanguage('fr')}>
+                            French
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+                <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
               </div>
 
               {/* Search */}
